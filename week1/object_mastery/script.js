abb74a4ab9eb3b0b3ca447f02742a42297239432
DuxEva <evariste.dusingizimana@amalitechtraining.org>
2024-06-23 20:59:05 +0200

    javascript hero object mastery

---
+++ week1/object_mastery/script.js
@@ -0,0 +1,112 @@
+const superHero = {
+  name: "Batman",
+  secretIdentity: "Bruce Wayne",
+  powers: ["Wealth", "Intelligence", "Combat", "Gadgets"],
+  weakness: ["Parents", "Guilt"],
+  usePower(powerName) {
+    if (this.powers.includes(powerName))
+      console.log(`${this.name} does have ${powerName} power!`);
+    else console.log(`${this.name} does not have ${powerName} power!`);
+  },
+  revealIdenty() {
+    console.log(
+      `I am ${this.name} and my secret identity is ${this.secretIdentity}.`
+    );
+  },
+};
+
+superHero.revealIdenty();
+superHero.usePower("Wealth");
+
+function Superhero(name, secretIdentity, powers, weakness) {
+  this.name = name;
+  this.secretIdentity = secretIdentity;
+  this.powers = powers;
+  this.weakness = weakness;
+}
+
+// const batman = new Superhero(
+//   "Spiderman",
+//   "Peter Parker",
+//   ["Webs", "Agility", "Spider-sense"],
+//   ["Mary Jane", "Guilt"]
+// );
+// console.log(batman);
+
+Superhero.prototype.usePower = function (powerName) {
+  if (this.powers.includes(powerName))
+    console.log(`${this.name} does have ${powerName} power!`);
+  else console.log(`${this.name} does not have ${powerName} power!`);
+};
+
+Superhero.prototype.revealIdenty = function () {
+  console.log(
+    `I am ${this.name} and my secret identity is ${this.secretIdentity}.`
+  );
+};
+
+// const superman = new Superhero(
+//   "Superman",
+//   "Clark Kent",
+//   ["Flight", "Strength", "Heat Vision"],
+//   ["Kryptonite", "Magic"]
+// );
+// console.log(superman);
+
+// superman.revealIdenty();
+
+const characters = [
+  new Superhero(
+    "Superman",
+    "Clark Kent",
+    ["Super Strength", "Flight"],
+    "Kryptonite"
+  ),
+  new Superhero(
+    "Batman",
+    "Bruce Wayne",
+    ["Martial Arts", "Detective Skills"],
+    "None"
+  ),
+  { name: " Iron Man", type: "hero" },
+  { name: "Musketeer", type: "hero" },
+];
+
+// Use filter to get only superheroes
+const filteredCharacters = characters.filter(
+  (character) => character instanceof Superhero
+);
+// console.log(filteredCharacters);
+
+// Use map to get names of characters
+const names = characters.map((character) => character.name);
+// console.log(names);
+
+// Using forEach to log names
+// characters.forEach((character) => console.log(character.name));
+
+function battle(hero1, hero2) {
+  console.log(`${hero1.name} and ${hero2.name} are battling!`);
+  // Simple battle logic
+  const winner = Math.random() > 0.5 ? hero1.name : hero2.name;
+  console.log(`${winner} wins!`);
+}
+
+const superman = new Superhero(
+  "Superman",
+  "Clark Kent",
+  ["Super Strength", "Flight"],
+  "Kryptonite"
+);
+const batman = new Superhero(
+  "Batman",
+  "Bruce Wayne",
+  ["Martial Arts", "Detective Skills"],
+  "None"
+);
+const wonderWoman = new Superhero(
+  "Wonder Woman",
+  "Diana Prince",
+  ["Super Strength", "Flight"],
+  "None"
+);
